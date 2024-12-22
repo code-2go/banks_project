33af8e73c86268adb4c8ab33a9621c74ccc82dd7
Bruno <bgurgelamaral@gmail.com>
2024-12-22 11:12:14 -0300

    The project has begun
    The first steps consists of creating a preliminary strutcture such as installing the libs, defining some variables and schemas.
    
    The first algorithm works to download a csv file.

---
+++ etl.process.py
@@ -0,0 +1,27 @@
+import requests
+import sqlite3
+import pandas as pd
+from bs4 import BeautifulSoup
+from datetime import datetime 
+
+url = r'https://web.archive.org/web/20230908091635 /https://en.wikipedia.org/wiki/List_of_largest_banks'
+FX = r'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-PY0221EN-Coursera/labs/v2/exchange_rate.csv'
+
+base_path = r'C:\Users\b_gur\OneDrive\Documentos\IBM\banks_project'
+log_path = rf'{base_path}\log\code_log.txt'
+csv_path = rf'{base_path}\csv_files'
+db_name = 'Banks.db'
+table_name = 'Largest_banks'
+table_schema = ['Name', 'MC_USD_Billion']
+conn_sql = sqlite3.Connection(rf'{base_path}\db_files\{db_name}')
+
+def donwload_file(path, file_name):
+    response = requests.get(FX)
+    if response.status_code == 200:
+        with open(f'{csv_path}{file_name}', 'wb') as file:
+            file.write(response.content)
+            print(f'Download was a Success.')
+    else:
+        print(f'Download Fail. Status Code: {response.status_code}')
+
+donwload_file(FX, '\FX_data.csv')
