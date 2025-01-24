f99858ce3923ac5114386e28f815c106336d3352
Bruno <bgurgelamaral@gmail.com>
2025-01-24 17:16:37 -0300

    Exceptions has been added
    Exception handling has been added to the function,
    such as FileNotFound

--- src/load/load_csv.py
+++ src/load/load_csv.py
@@ -2,9 +2,25 @@ from src.config import CSV_PATH
 import os
 
 def load_file_csv(df, file_name, layer='_raw'):
-    path = os.path.join(CSV_PATH, layer, file_name)
-    if not os.path.isfile(path):
-        df.to_csv(f"{path}.csv")
-        print(f'File saved in {layer} layer: {file_name}.')
-    else:
-        print(f'The file {file_name} already exists in {layer} layer.')
+    path = os.path.join(CSV_PATH, layer, f"{file_name}.csv")
+
+    try:
+        # check the existence of the directory
+        directory = os.path.dirname(path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        # check if the file exists
+        if not os.path.isfile(path):
+            df.to_csv(f"{path}.csv")
+            print(f'File saved in {layer} layer: {file_name}.')
+        else:
+            print(f'The file {file_name} already exists in {layer} layer.')
+        return True
+    
+    except FileNotFoundError as e:
+        print(f'The directory was not found')
+        return False
+    
+    except Exception as e:
+        print(f'An error occurred: {e}')
+        return False
