940835541add23ded251de9cffa8f8f75675f8df
Bruno <bgurgelamaral@gmail.com>
2025-01-25 20:37:10 -0300

    The load to csv function has been refactored
    Some details were fixed such as, ".csv" into
    df.to_csv(f"{path}.csv") has been removed and a
    conditional script to check if file_name has
    ".csv" at the end of its name

--- src/load/load_csv.py
+++ src/load/load_csv.py
@@ -1,26 +1,31 @@
 from src.config import CSV_PATH
 import os
 
-def load_file_csv(df, file_name, layer='_raw'):
-    path = os.path.join(CSV_PATH, layer, f"{file_name}.csv")
+def load_file_csv(df, file_name, layer='_raw', base_path=CSV_PATH):
+
+    if not file_name.endswith(".csv"):
+        file_name += ".csv"
+
+    path = os.path.join(base_path, layer, file_name)
+    
+    if os.path.exists(path):
+        print(f'The file {file_name}.csv already exists in {layer} layer.')
+        return False 
 
     try:
         # check the existence of the directory
         directory = os.path.dirname(path)
         if not os.path.exists(directory):
             os.makedirs(directory)
-        # check if the file exists
-        if not os.path.isfile(path):
-            df.to_csv(f"{path}.csv")
-            print(f'File saved in {layer} layer: {file_name}.')
-        else:
-            print(f'The file {file_name} already exists in {layer} layer.')
+
+        df.to_csv(f"{path}")
+        print(f'File saved in {layer} layer: {file_name}.')
         return True
     
     except FileNotFoundError as e:
-        print(f'The directory was not found')
-        return False
+        print(f'Required file not found: {e}. Check if the path is correct')
+        raise
     
     except Exception as e:
         print(f'An error occurred: {e}')
-        return False
+        raise
